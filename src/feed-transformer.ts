2b905af710d6c4aca2407eed834564e796a90aa8
Ruben Quast <Ruben.Quast@outlook.fr>
2026-03-22 22:12:13 +0100

    Introduced basic feed transform (following ETL pattern)

---
+++ src/feed-transformer.ts
@@ -0,0 +1,30 @@
+import { Item } from './feed-extractor';
+
+
+export function transform(items: Item[]): Item[] {
+    // Remove duplicate items (URL is used as a unique identifier)
+    const uniqueItems = items.filter((item, index, self) => {
+        return index === self.findIndex((t) => t.link === item.link);
+    });
+
+    // Limit items to the last 36 hours
+    const recentItems = uniqueItems.reduce((acc, item) => {
+        if (item.pubDate) {
+            const ageInHours = (Date.now() - Date.parse(item.pubDate)) / (1000 * 60 * 60);
+            if (ageInHours <= 36) acc.push(item);
+        }
+        return acc;
+    }, [] as Item[]);
+
+    // Limit number of items from each feed according to its own configuration
+    const limitedItems = recentItems.reduce((acc, item) => {
+        const sourceItems = acc.filter((i) => i.feedTitle === item.feedTitle);
+        const limitNumberTo = item.feedConfiguration.limitNumberTo;
+        if (!limitNumberTo || sourceItems.length < limitNumberTo) {
+            acc.push(item);
+        }
+        return acc;
+    }, [] as Item[]);
+
+    return limitedItems;
+}
